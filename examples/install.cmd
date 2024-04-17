213574fdcd7262bc569fb9f4fca5562203896911
elmasrisaer <139119606+elmasrisaer@users.noreply.github.com>
2024-04-17 15:13:47 +0300

    liblab SDK update for version v1.0.3

--- examples/install.cmd
+++ examples/install.cmd
@@ -2,5 +2,5 @@ python -m venv .venv
 call .venv\Scripts\activate
 pip install build
 python -m build --outdir dist ..\
-pip install dist\saer-1.0.2-py3-none-any.whl --force-reinstall
+pip install dist\saer-1.0.3-py3-none-any.whl --force-reinstall
 deactivate
